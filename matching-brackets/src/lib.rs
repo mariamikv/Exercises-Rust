c9f68c98faa4c165e73e899b90d469629f9d0093
mariamikv <mariamikv71@gmail.com>
2025-05-15 22:52:33 +0400

    marching-brackets

---
+++ matching-brackets/src/lib.rs
@@ -0,0 +1,28 @@
+pub fn brackets_are_balanced(input: &str) -> bool {
+    let mut stack = Vec::new();
+
+    for ch in input.chars() {
+        match ch {
+            '(' | '[' | '{' => stack.push(ch),
+            ')' => {
+                if stack.pop() != Some('(') {
+                    return false;
+                }
+            }
+            ']' => {
+                if stack.pop() != Some('[') {
+                    return false;
+                }
+            }
+            '}' => {
+                if stack.pop() != Some('{') {
+                    return false;
+                }
+            }
+            _ => {}
+        }
+    }
+
+    stack.is_empty()
+}
+
